e9358e8f7981abc1516524ed25679d271b979df0
jfigielst <j.figiel.st@spseiostrava.cz>
2026-05-18 21:24:23 +0200

    Add files via upload

---
+++ htdocs/kontrolery/Kontroler.php
@@ -0,0 +1,20 @@
+<?php
+abstract class Kontroler
+{
+    protected $pohled = ""; // název souboru s pohledem (bez přípony .phtml)
+    protected $data = []; // asociativní pole dat (např. pro předání z metody zpracuj() do pohledu)
+
+    abstract public function zpracuj($parametry);
+
+    public function vypisPohled()
+    {
+        extract($this->data); // vytvoří podle klíčů pole i samostatné proměnné
+        require "pohledy/{$this->pohled}.phtml";
+    }
+
+    public function presmeruj($url)
+    {
+        header("Location: /$url");
+        exit();
+    }
+}
---
+++ htdocs/modely/Db.php
@@ -0,0 +1,31 @@
+<?php
+
+class Db
+{
+
+    // Databázové spojení
+    private static $spojeni;
+
+    // Výchozí nastavení ovladače
+    private static $nastaveni = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    );
+
+    // Připojí se k databázi pomocí daných údajů
+    public static function pripoj($server, $uzivatel, $heslo, $databaze)
+    {
+        if (!isset(self::$spojeni)) {
+            $dsn = "mysql:host=$server;dbname=$databaze;charset=utf8";
+            self::$spojeni = new PDO(
+                $dsn,
+                $uzivatel,
+                $heslo,
+                self::$nastaveni
+            );
+        }
+    }
+
+
+
+}
